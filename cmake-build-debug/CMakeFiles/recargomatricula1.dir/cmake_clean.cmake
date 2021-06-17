a62083fee5c00b10957d413022757ecac99ada44
Ana Junco <anajuncoescobar@gmail.com>
2021-06-17 13:51:15 -0500

    Enunciado del ejercicio

---
+++ cmake-build-debug/CMakeFiles/recargomatricula1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recargomatricula1.dir/main.cpp.obj"
+  "librecargomatricula1.dll.a"
+  "recargomatricula1.exe"
+  "recargomatricula1.exe.manifest"
+  "recargomatricula1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recargomatricula1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//Se ingresa la edad de un estudiante. si la edad es de 5 a 10 años, se hace
+//un recargo del 1% al costo de la matricula. Si la edad es supeior a los 10 años
+//y menor o igual que 15 años, el recargo sera del 2%. Si la edad es mayor que
+//15 y menor o igual que 20, el recargo es del 4%. Si la edad es superior
+// a 20 años, el recargo es del 4%. Si la edad es superior
+//a 20 años,el recargo es del 6%.
+int main() {
+
+    return 0;
+}
